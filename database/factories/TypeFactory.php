7a821e203a2a61c97ade68105ff91507232077a6
milosevicognjen20221061 <milosevicognjen01@gmail.com>
2024-02-01 16:31:45 +0100

    factory

---
+++ database/factories/TypeFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Type>
+ */
+class TypeFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => fake()->randomElement([
+                'jpg', 'png', 'jpeg', 'txt', 'mp4', 'mp3',
+                'css', 'js', 'html', 'csv', 'xls', 'evac'
+            ])
+        ];
+    }
+}
